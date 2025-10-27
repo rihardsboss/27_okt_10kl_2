823d0a56b6fdcfaf0c44293a0ed80a45bda934b1
ZMGnv <73342220+ZMGnv@users.noreply.github.com>
2025-10-27 11:50:17 +0200

    Create uzd2.py
    Fork, izlabot kļūdas!

---
+++ uzd2.py
@@ -0,0 +1,6 @@
+battery= 20 # sākumā ir 20%
+# risinājumam izmantojam While konstrukciju
+    while battery<100:
+    print("Lādējam ...baterija ir", battery,"%")
+    battery=20 # katru reizi pieaug pa 20% ... battery=battery+20
+prit ("Telefons ir pilnībā uzlādēts!")
